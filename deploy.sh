b19a7c5a3c07b31d3bedbf2fc851e0844fce73b9
jayplin <teSvDPG77VVx9KR>
2019-10-10 21:10:17 +0200

    added travis ci

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:jayplin/pomodoro-vue.git master:gh-pages
+ git push -f https://github.com/jayplin/pomodoro-vue.git master:gh-pages
 
 cd -
